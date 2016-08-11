39bed78fec27f6530d7966b385e7daf584b1b9de
Gerard Braad <me@gbraad.nl>
2016-08-11 08:34:23 +0000

    Add docker role

--- _devel.sh
+++ _devel.sh
@@ -2,4 +2,5 @@
 mkdir -p roles
 git clone https://github.com/gbraad/ansible-role-k8s-master.git roles/gbraad.k8s-master
 git clone https://github.com/gbraad/ansible-role-k8s-node.git roles/gbraad.k8s-node
+git clone https://github.com/gbraad/ansible-role-docker.git roles/gbraad.docker
 git clone https://github.com/gbraad/ansible-role-docker-registry.git roles/gbraad.docker-registry
