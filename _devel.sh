bb11cd1d59ae24e57270c524abfa0c653b5a9be0
Gerard Braad <me@gbraad.nl>
2016-08-16 14:53:44 +0000

    Install client

--- _devel.sh
+++ _devel.sh
@@ -4,4 +4,6 @@ git clone https://github.com/gbraad/ansible-role-docker.git roles/gbraad.docker
 git clone https://github.com/gbraad/ansible-role-docker-registry.git roles/gbraad.docker-registry
 git clone https://github.com/gbraad/ansible-role-kubernetes-master.git roles/gbraad.kubernetes-master
 git clone https://github.com/gbraad/ansible-role-kubernetes-node.git roles/gbraad.kubernetes-node
+git clone https://github.com/gbraad/ansible-role-kubernetes-client.git roles/gbraad.kubernetes-client
+
 
